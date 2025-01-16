5d542d819347cf6dcf1660f955d4cbecde4d5e9c
djuk1ca <djukiuki@gmail.com>
2025-01-16 20:07:22 +0100

    Broker Auditor Home Page V2.01

--- src/pages/landingPage/components/Subsection.jsx
+++ src/pages/landingPage/components/Subsection.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 
 export default function Subsection() {
-    const [menuOpen, setMenuOpen] = useState(false)
+    const [menuOpen, setMenuOpen] = useState(true)
     return(
         <div className="flex flex-wrap w-[90%] md:w-4/5 m-auto py-8 text-white items-center justify-between">
                 <div className="w-full lg:w-[45%] text-center lg:text-left mb-8 lg:mb-0">
@@ -10,16 +10,16 @@ export default function Subsection() {
                 </div>
                 <div className="w-full lg:w-[45%] flex flex-col items-center lg:items-end lg:text-right">
                     <div className="w-full lg:w-[80%]">
-                        <button onClick={() => setMenuOpen(!menuOpen)} className="w-full cursor-pointer flex items-center justify-between py-3 text-[1.5rem] text-white uppercase font-bold px-4 lg:px-12 bg-gradient-to-b from-[#065967] to-[#dde26e] rounded-t-lg border-[2px] border-[#00818A] border-b-0">
+                        <button onClick={() => setMenuOpen(!menuOpen)} className={`${menuOpen && `rounded-b-none`} w-full cursor-pointer flex items-center justify-between py-2 text-[1.5rem] text-white uppercase font-bold px-4 lg:px-12 bg-gradient-to-b from-[#065967] to-[#dde26e] rounded-lg`}>
                             Report Types
                             <svg class="-mr-1 size-[2.5rem] text-white" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
                                 <path fill-rule="evenodd" d="M5.22 8.22a.75.75 0 0 1 1.06 0L10 11.94l3.72-3.72a.75.75 0 1 1 1.06 1.06l-4.25 4.25a.75.75 0 0 1-1.06 0L5.22 9.28a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd" />
                             </svg>
                             </button>
-                        <ul className={`${menuOpen ? `max-h-screen opacity-100` : `max-h-0 opacity-0`} 
+                        <ul className={`${menuOpen ? `max-h-screen opacity-100 rounded-t-none` : `max-h-0 opacity-0`} 
                                         transition-all duration-300 overflow-hidden 
                                         px-12 text-center lg:text-left w-full none py-3 font-semibold tracking-wider
-                                        bg-gradient-to-t border-[2px] border-[#00818A] border-t-0 from-[#065967] to-[#dde26e]`}>
+                                        bg-gradient-to-t from-[#065967] to-[#dde26e] rounded-b-lg`}>
                             <li >Filed False Carrier Report</li>
                             <li>Detention Unpaid</li>
                             <li>TONU Unpaid</li>
@@ -31,7 +31,20 @@ export default function Subsection() {
                             <li>Other</li>
                         </ul>
                     </div>
-                    
+                </div>
+                <div className="w-full flex flex-col items-center py-4">
+                    <div className="w-[85%] flex justify-center gap-4 items-center">
+                        <h1 className="text-[3rem] rotate-6 font-bold text-transparent bg-clip-text bg-gradient-to-t from-[#065967] to-[#dde26e]">1</h1>
+                        <p className="text-[1.25rem] lg:text-[2rem] text-white italic">File a report and evidence</p>
+                    </div>
+                    <div className="w-[85%] flex justify-center gap-4 items-center">
+                        <h1 className="text-[3rem] rotate-6 font-bold text-transparent bg-clip-text bg-gradient-to-b from-[#065967] to-[#dde26e]">2</h1>
+                        <p className="text-[1.25rem] lg:text-[2rem] text-white italic">We manually review the integrity</p>
+                    </div>
+                    <div className="w-[85%] flex justify-center gap-4 items-center">
+                        <h1 className="text-[3rem] rotate-6 font-bold text-transparent bg-clip-text bg-gradient-to-t from-[#065967] to-[#dde26e]">3</h1>
+                        <p className="text-[1.25rem] lg:text-[2rem] text-white italic">Report published to profile</p>
+                    </div>
                 </div>
         </div>
     )
