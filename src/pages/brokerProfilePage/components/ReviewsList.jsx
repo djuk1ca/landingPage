0f9d823e047f05a2d73111f8d7625be2e63aea16
djuk1ca <djukiuki@gmail.com>
2025-02-25 21:18:29 +0100

    semifinal

--- src/pages/brokerProfilePage/components/ReviewsList.jsx
+++ src/pages/brokerProfilePage/components/ReviewsList.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import Review from "./Review";
 
 export default function ReviewsList() {
@@ -7,7 +8,7 @@ export default function ReviewsList() {
         <div className="w-full bg-[#151516] border-[3px] rounded-t-[20px] border-[#252525] border-b-[#00818A] text-center text-white px-4 py-3 h-40 overflow-y-auto scroll-smooth scroll mb-10">
             <div className="flex justify-between pb-3">
                 <h1 className="text-[1rem] lg:text-[1.25rem]  pb-2 font-thin tracking-tight">Carrier Provided Reviews</h1>
-                <button className="px-4 py-[0.25rem] uppercase border-[1px] border-[#00818A] text-[#00818A] text-[0.75rem]">view all</button>
+                <Link to="/reviews"><button className="px-4 py-[0.25rem] uppercase border-[1px] border-[#00818A] text-[#00818A] text-[0.75rem]">View All</button></Link >
             </div>
 
             <Review name="Anonymus Carrier" rating={4} description="Wow, best broker I've ever dealt with." />
